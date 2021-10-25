9dfb8f636211e27a3c0135eef159e59fc7ebd4d0
Ahmad Jailani <ahmadjailani604@gmail.com>
2021-10-25 10:25:29 +0700

    Make the app just has light mode

--- app/src/main/java/com/ajailani/resepy/ui/theme/Theme.kt
+++ app/src/main/java/com/ajailani/resepy/ui/theme/Theme.kt
@@ -32,11 +32,7 @@ fun ResepMakananTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable() () -> Unit
 ) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+    val colors = LightColorPalette
 
     MaterialTheme(
         colors = colors,
